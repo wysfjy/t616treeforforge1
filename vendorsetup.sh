2f188ddf23e60091a6186e941f6a4cf583b36ffb
FengJiayan <fengjiayan0@gmail.com>
2025-12-07 17:45:46 +0800

    Update lunch combo options for M16 to version 13

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_M16-user
-add_lunch_combo omni_M16-userdebug
-add_lunch_combo omni_M16-eng
+add_lunch_combo omni_M16-13-user
+add_lunch_combo omni_M16-13-userdebug
+add_lunch_combo omni_M16-13-eng
